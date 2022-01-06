df1870b398c3750dfd4460156ec03d1b8a21e6a4
mfisher5 <mfisher5@uw.edu>
2022-01-06 13:02:12 -0800

    kelly lab scripts

---
+++ test-lane-1/scripts/blast.md
@@ -0,0 +1,17 @@
+## Blast ASV sequences
+
+### Install blast
+
+### Get blast database
+
+The Kelly lab appears to use the preformatted 'nt' or nucleotide database. To download a preformatted NCBI BLAST database, run the update_blastdb.pl program followed by any relevant options and the name(s) of the BLAST databases to download. For example:
+
+```
+$ update_blastdb.pl --decompress nt [*]
+```
+
+This command will download the compressed nr BLAST database from NCBI to the current working directory and decompress it. Any subsequent identical invocations of this script with the same parameters in that directory will only download any data if it has a different time stamp when compared to the data at NCBI.
+
+
+### 
+
---
+++ test-lane-1/scripts/blast_BF3_2022-01-06.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PATH=$PATH:/mnt/nfs/home/rpkelly/ncbi-blast-2.8.1+/bin
+export BLASTDB="/mnt/nfs/home/rpkelly/blastdb"
+
+BLAST_DB='/mnt/nfs/home/rpkelly/blastdb/nt'
+# BLAST PARAMETERS
+PERCENT_IDENTITY="85"
+WORD_SIZE="30"
+EVALUE="1e-30"
+# number of matches recorded in the alignment:
+MAXIMUM_MATCHES="50"
+CULLING="5"
+
+	################################################################################
+	# BLAST CLUSTERS
+	################################################################################
+	echo $(date +%H:%M) "BLASTing..."
+
+	blast_output="/mnt/nfs/home/rpkelly/processed/run2.COI.hashes.to.blast.txt" 
+	blastn \
+		-query "/c/Users/mcf05/Documents/PhD/DNA_Metabarcoding/GreenCrab/EGC-Salish-Sea/test-lane-1/data/BF3/hash_key.fasta
+" \
+		-db "${BLAST_DB}" \
+		-num_threads 16 \
+		-perc_identity "${PERCENT_IDENTITY}" \
+		-word_size "${WORD_SIZE}" \
+		-evalue "${EVALUE}" \
+		-max_target_seqs "${MAXIMUM_MATCHES}" \
+		-culling_limit="${CULLING}" \
+		-outfmt "6 qseqid sseqid sacc pident length mismatch gapopen qcovus qstart qend sstart send evalue bitscore staxids qlen sscinames sseq" \
+		-out "${blast_output}"
+
+
+	echo $(date +%H:%M) "BLASTing second batch..."
+
+	blast_output="/mnt/nfs/home/rpkelly/processed/run2.COI.hashes.to.blast2.txt" 
+	blastn \
+		-query "/mnt/nfs/home/rpkelly/raw/run2.COI.hashes.to.blast2.fasta" \
+		-db "${BLAST_DB}" \
+		-num_threads 16 \
+		-perc_identity "${PERCENT_IDENTITY}" \
+		-word_size "${WORD_SIZE}" \
+		-evalue "${EVALUE}" \
+		-max_target_seqs "${MAXIMUM_MATCHES}" \
+		-culling_limit="${CULLING}" \
+		-outfmt "6 qseqid sseqid sacc pident length mismatch gapopen qcovus qstart qend sstart send evalue bitscore staxids qlen sscinames sseq" \
+		-out "${blast_output}"
