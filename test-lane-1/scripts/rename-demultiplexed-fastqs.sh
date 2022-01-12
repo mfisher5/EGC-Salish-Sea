b4dbcb326817f948a2bcda6e998698eeb4a3bbd3
mfisher5 <mfisher5@uw.edu>
2022-01-12 08:49:27 -0800

    rename fastq files

---
+++ test-lane-1/scripts/rename-demultiplexed-fastqs.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# forward
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_062VA_S28_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_062VA_S28_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_062VB_S29_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_062VB_S29_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_062VC_S30_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_062VC_S30_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_070VA_S31_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_070VA_S31_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_070VB_S32_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_070VB_S32_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_070VC_S33_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_070VC_S33_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_071VA_S34_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_071VA_S34_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_071VB_S35_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_071VB_S35_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_071VC_S36_L001_R1_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_071VC_S36_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_01a_S55_L001_R1_001.fastq.gz data/raw/lane_1/Leray_MARPT_01a_S55_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_01b_S56_L001_R1_001.fastq.gz data/raw/lane_1/Leray_MARPT_01b_S56_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_02a_S57_L001_R1_001.fastq.gz data/raw/lane_1/Leray_MARPT_02a_S57_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_02b_S58_L001_R1_001.fastq.gz data/raw/lane_1/Leray_MARPT_02b_S58_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_062VA_S37_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_062VA_S37_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_062VB_S38_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_062VB_S38_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_062VC_S39_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_062VC_S39_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_070VA_S40_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_070VA_S40_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_070VB_S41_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_070VB_S41_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_070VC_S42_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_070VC_S42_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_071VA_S43_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_071VA_S43_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_071VB_S44_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_071VB_S44_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_071VC_S45_L001_R1_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_071VC_S45_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_MARPT_01a_S59_L001_R1_001.fastq.gz data/raw/lane_1/BF3_MARPT_01a_S59_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_MARPT_01b_S60_L001_R1_001.fastq.gz data/raw/lane_1/BF3_MARPT_01b_S60_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/NA_S61_L001_R1_001.fastq.gz data/raw/lane_1/BF3_MARPT_02a_S61_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/NA_S62_L001_R1_001.fastq.gz data/raw/lane_1/BF3_MARPT_02b_S62_L001_R1_001.fastq.gz
+cp data/raw/lane_1/misnamed/Undetermined_S0_L001_R1_001.fastq.gz data/raw/lane_1/Leray_kangaroo_S103_L001_R1_001.fastq.gz
+
+
+# reverse
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_062VA_S28_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_062VA_S28_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_062VB_S29_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_062VB_S29_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_062VC_S30_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_062VC_S30_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_070VA_S31_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_070VA_S31_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_070VB_S32_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_070VB_S32_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_070VC_S33_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_070VC_S33_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_071VA_S34_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_071VA_S34_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_071VB_S35_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_071VB_S35_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_WASS_2018_071VC_S36_L001_R2_001.fastq.gz data/raw/lane_1/Leray_WASS_2018_071VC_S36_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_01a_S55_L001_R2_001.fastq.gz data/raw/lane_1/Leray_MARPT_01a_S55_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_01b_S56_L001_R2_001.fastq.gz data/raw/lane_1/Leray_MARPT_01b_S56_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_02a_S57_L001_R2_001.fastq.gz data/raw/lane_1/Leray_MARPT_02a_S57_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/BF3_MARPT_02b_S58_L001_R2_001.fastq.gz data/raw/lane_1/Leray_MARPT_02b_S58_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_062VA_S37_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_062VA_S37_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_062VB_S38_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_062VB_S38_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_062VC_S39_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_062VC_S39_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_070VA_S40_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_070VA_S40_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_070VB_S41_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_070VB_S41_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_070VC_S42_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_070VC_S42_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_071VA_S43_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_071VA_S43_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_071VB_S44_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_071VB_S44_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_WASS_2018_071VC_S45_L001_R2_001.fastq.gz data/raw/lane_1/BF3_WASS_2018_071VC_S45_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_MARPT_01a_S59_L001_R2_001.fastq.gz data/raw/lane_1/BF3_MARPT_01a_S59_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Leray_MARPT_01b_S60_L001_R2_001.fastq.gz data/raw/lane_1/BF3_MARPT_01b_S60_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/NA_S61_L001_R2_001.fastq.gz data/raw/lane_1/BF3_MARPT_02a_S61_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/NA_S62_L001_R2_001.fastq.gz data/raw/lane_1/BF3_MARPT_02b_S62_L001_R2_001.fastq.gz
+cp data/raw/lane_1/misnamed/Undetermined_S0_L001_R2_001.fastq.gz data/raw/lane_1/Leray_kangaroo_S103_L001_R2_001.fastq.gz
