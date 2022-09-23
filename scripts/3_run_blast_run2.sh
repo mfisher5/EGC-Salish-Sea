50c1984828acc3e54f3bdd4845fd649c2b648aba
Mary Fisher <mfisher5@uw.edu>
2022-09-23 11:39:56 -0700

    run dada2

--- scripts/3_run_blast_run2.sh
+++ scripts/3_run_blast_run2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-PATH=$PATH:/mnt/c/Users/mfisher5/Documents/sci_programs/ncbi-blast-2.13.0+/bin
-export BLASTDB="/mnt/c/Users/mfisher5/Documents/sci_programs/ncbi-blast-2.13.0+/blastdb"
-BLAST_DB='/mnt/c/Users/mfisher5/Documents/sci_programs/ncbi-blast-2.13.0+/blastdb/nt'
-blast_output='/mnt/c/Users/mfisher5/Documents/EGC-Willapa-Bay/data/blast/run_2/hash_key_blast_2022-06-20.fasta'
+PATH=$PATH:/mnt/c/Users/mfisher5/Documents/my_programs/ncbi-blast-2.13.0+/bin
+export BLASTDB="/mnt/c/Users/mfisher5/Documents/my_programs/ncbi-blast-2.13.0+/blastdb"
+BLAST_DB='/mnt/c/Users/mfisher5/Documents/my_programs/ncbi-blast-2.13.0+/blastdb/nt'
+blast_output='/mnt/c/Users/mfisher5/Documents/EGC-Salish-Sea/data/blast/hash_key_blast_2022-09-23.fasta'
 # BLAST PARAMETERS
 PERCENT_IDENTITY="85"
 WORD_SIZE="30"
@@ -14,4 +14,4 @@ CULLING="5"
 # BLAST CLUSTERS
 ################################################################################
 echo $(date +%H:%M) "BLASTing..."
-./blastn -query "/mnt/c/Users/mfisher5/Documents/EGC-Willapa-Bay/data/dada2/run_2/hash_key.fasta" -db "${BLAST_DB}" -num_threads 16 -perc_identity "${PERCENT_IDENTITY}" -word_size "${WORD_SIZE}" -evalue "${EVALUE}" -max_target_seqs "${MAXIMUM_MATCHES}" -culling_limit="${CULLING}" -outfmt "6 qseqid sseqid sacc pident length mismatch gapopen qcovus qstart qend sstart send evalue bitscore staxids qlen sscinames sseq" -out "${blast_output}"
+./blastn -query "/mnt/c/Users/mfisher5/Documents/EGC-Salish-Sea/data/dada2/hash_key.fasta" -db "${BLAST_DB}" -num_threads 16 -perc_identity "${PERCENT_IDENTITY}" -word_size "${WORD_SIZE}" -evalue "${EVALUE}" -max_target_seqs "${MAXIMUM_MATCHES}" -culling_limit="${CULLING}" -outfmt "6 qseqid sseqid sacc pident length mismatch gapopen qcovus qstart qend sstart send evalue bitscore staxids qlen sscinames sseq" -out "${blast_output}"
